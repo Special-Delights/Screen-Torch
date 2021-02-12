31ac3b017c284e6aa97fdd6e86763101ae17c93d
Stabaho <stabaho@gmail.com>
2021-02-11 23:15:22 -0600

    Simple switch from white to red

--- Screen Torch/ViewController.swift
+++ Screen Torch/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var viewColorFlipped = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if !viewColorFlipped {
+            view.backgroundColor = UIColor.red
+        } else {
+        view.backgroundColor = UIColor.white
+    }
+        viewColorFlipped = !viewColorFlipped
+    
 }
-
+}
